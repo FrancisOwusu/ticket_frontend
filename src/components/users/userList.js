c018234a35b470fa6fce8cb954afedfb1b856972
FrancisOwusu <osei1257@gmail.com>
2024-10-12 15:24:43 +0000

    add userForm.js

--- src/components/users/userList.js
+++ src/components/users/userList.js
@@ -1,28 +1,48 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import UserService from "../../services/userService";
+import UserForm from "./userForm";
 const UserList = () => {
   const [users, setUsers] = useState([]);
-  //   const [editingUser, setEditingUser] = useState(null);
+  const [editingUser, setEditingUser] = useState(null);
 
-  const fetechUsers = async () => {
+  const fetchUsers = async () => {
     const response = await UserService.getUsers();
-    console.log(response.data)
     setUsers(response.data);
   };
+
+  const handleEdit = (user) => {
+    setEditingUser(user);
+  };
+
+  const handleDelete = async (userId) => {
+    await UserService.deleteUser(userId);
+    fetchUsers(); // Refresh the list after deletion
+  };
+
+  const handleSave = () => {
+    setEditingUser(null);
+    fetchUsers(); // Refresh the list after save
+  };
   useEffect(() => {
-    fetechUsers();
+    fetchUsers();
   }, []);
 
   return (
     <div>
       <h2>User Management</h2>
+      <UserForm
+        user={editingUser}
+        onSave={handleSave}
+        isEditing={!!editingUser}
+      />
+
       <h3>User List</h3>
       <ul>
-        {users.map((user) => (
-          <li key={user.user_id}>
+        {users.map((user, index) => (
+          <li key={index}>
             {user.first_name} {user.last_name} ({user.email}) - {user.status}
-            {/* <button onClick={() => handleEdit(user)}>Edit</button>
-              <button onClick={() => handleDelete(user.user_id)}>Delete</button> */}
+            <button onClick={() => handleEdit(user)}>Edit</button>
+            <button onClick={() => handleDelete(user.user_id)}>Delete</button>
           </li>
         ))}
       </ul>
